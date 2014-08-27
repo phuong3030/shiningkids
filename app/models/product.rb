232348b4bc6403442c68d1e25dfd44c82961f732
Tran Tuan Phuong <phuongtt@mmj.vn>
2014-08-27 22:36:19 +0700

    Product relation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :order
   has_many :product_galery
+
+  validates :name, :presence => true, :uniqueness => { case_sensitive: false }
 end
