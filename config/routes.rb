542f982d950a31704ea97e9ee83a7a9ac00c118b
Tran Tuan Phuong <phuongtt@mmj.vn>
2014-08-30 11:58:12 +0700

    Homepage partial view: header, footer, banner, slider added

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,27 @@ Rails.application.routes.draw do
 	devise_for :admin, :skip => [:registrations] 
 
   namespace :admin do
+    # CRUD nested category, product, image
     resources :categories do 
       resources :products do 
         resources :product_images, :as => :images
       end
     end
+
+    # View orders route
+
+  end
+
+  namespace :customer do 
+    get '/my-account', :to => 'account#my_account'
+
+    get '/cart', :to => 'order#cart'
+    get '/checkout', :to => 'order#checkout'
+    post '/checkout', :to => 'order#make_an_order'
   end
 
 	scope :module => :home do
-    root :to => "home#index"
+    root :to => 'home#index'
 	end
 
 end
